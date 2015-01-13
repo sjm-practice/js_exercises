ee25fdba6f2876be377007994d9183c74b1720cc
stevenjmarsh <sjm.atl@gmail.com>
2015-01-12 17:20:19 -0800

    fixing count leaves, and changing protoype object syntax

--- binary_search_2nd.js
+++ binary_search_2nd.js
@@ -11,14 +11,58 @@ function BinaryTree(initialValue) {
 }
 
 // Define useful methods for BinaryTree prototype
-BinaryTree.prototype.hasNoChildren = function() {
-  return this.leftChild === null && this.rightChild === null;
+
+// PRE coding pseudo code...
+// if new empty node, or found an equal value, save in current node
+// else                                   (prevents duplicate values)
+// if newval < treeNode.val, look left
+//    if nothing left, add left
+//    else go left, check again
+// else (look right)
+//    if nothing right, add right
+//    else go right, check again
+BinaryTree.prototype = {
+  insertValue: function(newValue) {
+    if (this.value === null || this.value === newValue) {
+      this.value = newValue;
+    } else {
+      if (newValue < this.value) {
+        if (this.leftChild === null) {
+          this.leftChild = new BinaryTree(newValue);
+        } else {
+          this.leftChild.insertValue(newValue);
+        }
+      } else {
+        if (this.rightChild === null) {
+          this.rightChild = new BinaryTree(newValue);
+        } else {
+          this.rightChild.insertValue(newValue);
+        };
+      }
+    }
+
+    return;
+  }
 }
 
-BinaryTree.prototype.insertValue = function(newValue) {
-  if (this.value === null) {
-    this.value = newValue;
+// use recursion to count the number of nodes
+// adding all left nodes to all right nodes, using stack to store sum
+function countLeafNodes(node) {
+  if (node === null) {
+    return 0;
   }
 
-  return;
+  if (node.leftChild === null && node.rightChild === null) {
+    return 1;
+  }
+
+  return countLeafNodes(node.leftChild) + countLeafNodes(node.rightChild);
+}
+
+var binaryTree = new BinaryTree();
+
+for (var i = 2; i < process.argv.length; i++) {
+  binaryTree.insertValue(process.argv[i]);
 };
+
+console.log("number of leaf nodes:", countLeafNodes(binaryTree));
