5da71f3480cf54e57c956a7a5b32146bac997da7
stevenjmarsh <sjm.atl@gmail.com>
2015-01-21 15:10:05 -0800

    finishing merge algorithm, needed to refer to solution

--- meeting_times.js
+++ meeting_times.js
@@ -1,12 +1,51 @@
 var meetingTimes = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]];
 
+//
+// fucking douche. the interviewcake problem stated the times in the array are not guaranteed
+// in order. I interpreted that as a requirement, where you can't sort the
+// array first. His solution sorts the array. Which is much simpler.
+// [maybe the myopic interpretation is my problem. but I was pissed none the less]
+//
+
+
 function condensedMeetingTimes(meetingTimes) {
   var condensedTimes = [],
-      meetingTimesLength = meetingTimes.length;
+      meetingTimesLength = meetingTimes.length,
+      previousStart,
+      previousEnd,
+      currentStart,
+      currentEnd;
 
-  for (var i = 0; i < meetingTimesLength; i++) {
-    condensedTimes[i] = meetingTimes[i];
-  };
+  // if there is only one element (or none), no work to be done
+  if (meetingTimesLength < 2) {
+    return meetingTimes;
+  }
+
+  // first sort the array by meeting start times
+  meetingTimes.sort(function(a, b) {
+    return a[0] - b[0];
+  });
+
+  // in order to be able to combine multiple meetings, need to 
+  // keep track of running start times and end times independent of meeting
+  // (can't compare adjacent meetings alone, may span over them)
+  previousStart = meetingTimes[0][0];
+  previousEnd = meetingTimes[0][1];
+
+  for (var i = 1; i < meetingTimesLength; i++) {
+    currentStart = meetingTimes[i][0];
+    currentEnd = meetingTimes[i][1];
+
+    if (currentStart <= previousEnd) {
+      previousEnd = previousEnd >= currentEnd ? previousEnd : currentEnd; // (keep max of the two)
+    } else {
+      condensedTimes.push([previousStart, previousEnd]);
+      previousStart = currentStart;
+      previousEnd = currentEnd;
+    }
+  }
+  
+  condensedTimes.push([previousStart, previousEnd]);  // store last meeting
 
   return condensedTimes;
 }
